bbb81479192a7c48d132a949bf4c3aa6538c6de5
jhaller19 <jhaller0899@gmail.com>
2022-10-09 16:49:42 -0400

    a

--- 2/dfs_queens.py
+++ 2/dfs_queens.py
@@ -10,6 +10,8 @@ This is the notebook version of the code. I will use this to explain the homewor
 
 As we did in class, we will represent the board as a one-dimensional array where each position in the arrray is the n'th queen's column value. So if the array is: [1, 3, 0, 2], then the first queen is in position 1 (from 0--3), the second queen is in position 3 (the last column), the third queen is in the first column and the last queen is the in the second position.
 """
+import math
+from shutil import move
 
 columns = [] #columns is the locations for each of the queens
 # columns[r] is a number c if a queen is placed at row r and column c.
@@ -29,19 +31,24 @@ def place_n_queens(size):
 place_n_queens(size)
 
 """Now, we can print the result with a simple loop:"""
-
+def displayBoard(columns):
+    for row in range(len(columns)):
+        for column in range(size):
+            if column == columns[row]:
+                print('X', end=' ')
+            else:
+                print(' .', end=' ')
+        print()
+    print(columns)
 def display():
     for row in range(len(columns)):
         for column in range(size):
             if column == columns[row]:
-                print('♛', end=' ')
+                print('X', end=' ')
             else:
                 print(' .', end=' ')
         print()
 
-place_n_queens(size)
-display()
-print(columns)
 
 """This of course is not necessary legal, so we'll write a simple DFS search with backtracking:"""
 
@@ -60,6 +67,7 @@ def solve_queen(size):
         print(number_of_moves)'''
         while column < size:
             number_of_iterations+=1
+            number_of_moves += 1
             if next_row_is_safe(column):
                 place_in_next_row(column)
                 row += 1
@@ -70,11 +78,12 @@ def solve_queen(size):
         # if I could not find an open column or if board is full
         if (column == size or row == size):
             number_of_iterations+=1
+            number_of_moves +=1
             # if board is full, we have a solution
             if row == size:
                 print("I did it! Here is my solution")
                 display()
-                #print(number_of_moves)
+                print(str(number_of_iterations) + ", " + str(number_of_moves))
                 return number_of_iterations, number_of_moves
             # I couldn't find a solution so I now backtrack
             prev_column = remove_in_current_row()
@@ -130,10 +139,250 @@ def next_row_is_safe(column):
 #size = int(input('Enter n: '))
 num_iterations=0
 number_moves = 0
-#for i in range(0, 100):
-#    columns = [] #columns is the locations for each of the queens
-num_iterations, number_moves=solve_queen(size)
-print(num_iterations)
-print(columns)
 
-"""Now what?  Can you implement the British Museum Algorithm?  How many iterations did it take to solve the 4 queens problem?  How many did it take to solve the 8 queens (if at all)?"""
+num_iterations, number_moves=solve_queen(size)
+
+######################################British Museum###########################################
+print('*****British Museum********')
+def checkValid(columns):
+    valid = True
+    checkCols = columns.copy()
+    columns.clear()
+    for i in checkCols:
+        valid &= next_row_is_safe(i)
+        if(not valid):
+            return valid
+        place_in_next_row(i)
+    return valid
+
+def britishMuseum(columns):
+    valid = False
+    while(not valid):
+        place_n_queens(size)
+        valid = checkValid(columns)
+#britishMuseum(columns)
+#displayBoard(columns,size)
+
+#####################################################################################################
+def countAttacks(columns):
+    # check column
+    c = 0
+    row = 0
+    for column in columns:
+        
+        for i in range(len(columns)):
+            if column == columns[i] and i != row:
+                c+=1
+    
+        # check diagonal
+        for queen_row, queen_column in enumerate(columns):
+            if queen_column - queen_row == column - row and (queen_column != column and queen_row != row):
+                c+=1
+
+    
+        # check other diagonal
+        for queen_row, queen_column in enumerate(columns):
+            if ((size - queen_column) - queen_row
+                == (size - column) - row) and (queen_column != column and queen_row != row):
+                c+=1
+        row+=1
+    
+    return c/2
+
+#columns = [3,1,0,2]
+#print(countAttacks(columns))
+
+##############Hill Climbing###########
+def arEqual(ar1,ar2):
+    for i in range(len(ar1)):
+        if(ar1[i] != ar2[i]):
+            return False
+    return True
+def rePlaceQueens(columns,size):
+    columns.clear()
+    row = 0
+    while row < size:
+        column=random.randrange(0,size)
+        columns.append(column)
+        row+=1
+def displayBoard(columns):
+    for row in range(len(columns)):
+        for column in range(size):
+            if column == columns[row]:
+                print('X', end=' ')
+            else:
+                print(' .', end=' ')
+        print()
+    print(columns)
+
+
+
+place_n_queens(size)
+def hillClimbing(columns):
+    moves = len(columns)
+    rePlaceQueens(columns,len(columns))
+    prevH = countAttacks(columns)
+    min = prevH
+    iterations = 0
+    while True:
+        iterations+=1
+        copyOfColumns = columns.copy()
+        curMinArray = columns.copy()
+        for i in range(len(columns)):
+            for j in range(len(columns)):
+                copyOfColumns[i] = j
+                if(not arEqual(copyOfColumns,columns)):
+                    h = countAttacks(copyOfColumns)
+                    if(h == 0):
+                        columns = copyOfColumns
+                        print(str(iterations) + ", " + str(moves))
+                        return copyOfColumns
+                    if(h <= min):
+                        min = h
+                        curMinArray = copyOfColumns.copy()
+        if(prevH == min):
+            #print("restart " + str(min))
+            rePlaceQueens(columns,size)
+            moves += len(columns)
+            prevH = countAttacks(columns)
+            min = prevH
+            continue
+        moves+=1
+        prevH = min
+        columns = curMinArray
+print("********Hill Climbing*************")
+winner = hillClimbing(columns)
+displayBoard(winner)
+
+######################################Forward Checking############################################3
+def next_row_is_safe_FC(column, temp):
+    row = len(columns) 
+    # check column
+    if temp[row][column] == 1:
+        return False
+    for queen_column in columns:
+        if column == queen_column:
+            return False
+    return True
+
+def place_in_next_row_FC(column,row,temp,columns):
+    columns.append(column)
+    for i in range(len(temp)):
+        temp[i][column] = 1
+    r = row
+    c = column
+    while True:
+        if(r > len(temp) - 1 or c > len(temp) - 1):
+            break
+        temp[r][c] = 1
+        r+=1
+        c+=1
+    r = row
+    c = column
+    while True:
+        if(r < 0 or c < 0):
+            break
+        temp[r][c] = 1
+        r-=1
+        c-=1
+    r = row
+    c = column
+    while True:
+        if(r > len(temp) - 1 or c < 0):
+            break
+        temp[r][c] = 1
+        r+=1
+        c-=1
+    r = row
+    c = column
+    while True:
+        if(c > len(temp) - 1 or r < 0):
+            break
+        temp[r][c] = 1
+        c+=1
+        r-=1
+    
+def remove_in_current_row_FC(column,row,temp,columns):
+    if len(columns) > 0:
+        for i in range(len(temp)):
+            temp[i][column] = 0
+        r = row
+        c = column
+        while True:
+            if(r > len(temp) - 1 or c > len(temp) - 1):
+                break
+            temp[r][c] = 0
+            r+=1
+            c+=1
+        r = row
+        c = column
+        while True:
+            if(r < 0 or c < 0):
+                break
+            temp[r][c] = 0
+            r-=1
+            c-=1
+        r = row
+        c = column
+        while True:
+            if(r > len(temp) - 1 or c < 0):
+                break
+            temp[r][c] = 0
+            r+=1
+            c-=1
+        r = row
+        c = column
+        while True:
+            if(c > len(temp) - 1 or r < 0):
+                break
+            temp[r][c] = 0
+            c+=1
+            r-=1
+        return columns.pop()
+    return -1    
+
+def forwardChecking(columns):
+    temp = [ [ 0 for i in range(size) ] for j in range(size) ]
+    columns.clear()
+    number_of_moves = 0 #where do I change this so it counts the number of Queen moves?
+    number_of_iterations = 0  
+    row = 0
+    column = 0
+    # iterate over rows of board
+    while True:
+        #place queen in next row
+        ''''print(columns)
+        print("I have ", row, " number of queens put down")
+        display()
+        print(number_of_moves)'''
+        while column < size:
+            number_of_iterations+=1
+            number_of_moves += 1
+            if next_row_is_safe_FC(column,temp):
+                place_in_next_row_FC(column,row,temp,columns)
+                row += 1
+                column = 0
+                break
+            else:
+                column += 1
+        # if I could not find an open column or if board is full
+        if (column == size or row == size):
+            number_of_iterations+=1
+            number_of_moves +=1
+            # if board is full, we have a solution
+            if row == size:
+                print("I did it! Here is my solution")
+                displayBoard(columns)
+                print(str(number_of_iterations) + ", " + str(number_of_moves))
+                return number_of_iterations, number_of_moves
+            # I couldn't find a solution so I now backtrack
+            prev_column = remove_in_current_row_FC(columns[row - 1],row - 1,temp,columns)
+            if (prev_column == -1): #I backtracked past column 1
+                print("There are no solutions")
+                #print(number_of_moves)
+                return number_of_iterations, number_of_moves
+            # try previous row again
+            row -= 1
+            # start checking at column = (1 + value of column in previous row)
+            column = 1 + prev_column
+forwardChecking(columns)
